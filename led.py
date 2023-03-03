43e55abcd49dc9f23367819823e21c6235791b57
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-03-03 00:31:04 -0600

    Intento4Nuevo28

--- led.py
+++ led.py
@@ -3,16 +3,17 @@ from gpiozero import LED
 
 
 class Led:
+    estado_actual=False
     def __init__(self, pin):
         self.pin = pin
         self.led = LED(self.pin)
-        self.estado_actual = False  # establecer el estado inicial como apagado
 
     def toggle(self):
         if self.estado_actual:
             self.led.off()
             self.estado_actual = False
             return 0
+
         else:
             self.led.on()
             self.estado_actual = True
