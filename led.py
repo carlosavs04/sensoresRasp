3305ee0429641c9d2b28e5fab71d6bc9cb071958
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-03-02 23:58:48 -0600

    Intento4Nuevo14

--- led.py
+++ led.py
@@ -6,13 +6,16 @@ class Led:
     def __init__(self, pin):
         self.pin = pin
         self.led = gpiozero.LED(self.pin)
-
+        self.estado = self.led.value
     def toggle(self):
         ban=0
-        if gpiozero.LED(self.pin).is_lit:
+        if self.estado == 1:
             self.led.on()
             ban = 1
         else:
             self.led.off()
             ban = 0
         return ban
+if __name__ == "__main__":
+    led1=Led(17)
+    print(led1.estado())
