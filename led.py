a39ed2fdc52c15a2bf1e2100ddb1e57d8872fd6c
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-03-03 00:28:54 -0600

    Intento4Nuevo27

--- led.py
+++ led.py
@@ -6,14 +6,18 @@ class Led:
     def __init__(self, pin):
         self.pin = pin
         self.led = LED(self.pin)
-    def toggle(self):
-        if self.led.is_active:
-            self.led.off()
-            return 0
+        self.estado_actual = False  # establecer el estado inicial como apagado
 
+    def toggle(self):
+        if self.estado_actual:
+            self.led.off()
+            self.estado_actual = False
+            return 0
         else:
             self.led.on()
+            self.estado_actual = True
             return 1
+
 
 
 
