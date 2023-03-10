f5f6dfe6dec56065fab654b1c136c62c177b30ef
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2023-03-10 12:05:00 -0600

    Prueba6Intento30 Ya insertar mongo

--- main.py
+++ main.py
@@ -55,6 +55,7 @@ class main:
                 for i in data:
                     # print(i)
                     # i es el json
+                    print(i)
                     sensor1 = Sensores(i["path"],i["nombre"],i["tipo"],i["valores"],i["dato"],i["fecha"],i["hora"],i["pines"])
                     print(sensor1.to_dict())
                     if len(i["pines"]) == 1:
