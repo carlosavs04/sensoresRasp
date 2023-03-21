0717964b675a87343ec3af4738d1d542d95e50e1
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-03-20 23:19:45 -0600

    Update Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -1,11 +1,11 @@
 import Adafruit_DHT
 
-class temperatura:
+class Temperatura:
     def __init__(self, pin):
         self.sensor = Adafruit_DHT.DHT11
         self.pin = pin
 
-    def lectura(self):
+    def medirTemperatura(self):
         hum, temp = Adafruit_DHT.read(self.sensor, self.pin)
         return  hum, temp
         
