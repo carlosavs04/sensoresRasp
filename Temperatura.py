fefc4055dab2b662abf579d86bad6e0af77f7852
carlosavs04 <109000628+carlosavs04@users.noreply.github.com>
2023-03-20 23:18:57 -0600

    Update Temperatura.py

--- Temperatura.py
+++ Temperatura.py
@@ -1,27 +1,13 @@
-import adafruit_dht
-import time
+import Adafruit_DHT
 
-class Temperatura:
+class temperatura:
     def __init__(self, pin):
-        self.dhtDevice = adafruit_dht.DHT11(pin)
+        self.sensor = Adafruit_DHT.DHT11
+        self.pin = pin
 
-    def medirTemperatura(self):
-        try:
-            temperatura, humedad = self.dhtDevice.temperature, self.dhtDevice.humidity
-            return temperatura, humedad
-        except RuntimeError as error:
-            print(error.args[0])
-            time.sleep(2.0)
-            return None
-        except Exception as error:
-            self.dhtDevice.exit()
-            raise error
-        
-if __name__ == "__main__":
-    sensor = Temperatura(4)
-    temperatura, humedad = sensor.medirTemperatura()
-    print("Temperatura: {} C".format(temperatura))
-    print("Humedad: {} %".format(humedad))
+    def lectura(self):
+        hum, temp = Adafruit_DHT.read(self.sensor, self.pin)
+        return  hum, temp
         
 
 
